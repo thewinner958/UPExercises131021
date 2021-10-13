bcac2eae40e7d0d6e29db262e19251ed6d9cbaf3
TheWinner <petergolevski@outlook.com>
2021-10-13 20:20:39 +0300

    aaand i found my answer for exercise 5

--- UPPract13.10.21.cpp
+++ UPPract13.10.21.cpp
@@ -66,7 +66,7 @@ int main()
     int num1, num2;
     cin >> num1 >> num2;
     bool divident = num1 >= num2;
-    cout << divident;
+    cout << boolalpha << divident << endl;
 
     //Задача 6 
     double side1, side2, side3;
